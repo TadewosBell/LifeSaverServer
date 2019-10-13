dd399bd68acf060f030eeae2f20d0408ad30a4de
ssandif <ssandifer@umbc.edu>
2019-10-12 21:13:47 -0400

    Add example current call retrieval

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
+from users.priority_level import PriorityLevel
 
 app = Flask(__name__)
 CORS(app)
@@ -25,6 +26,20 @@ def returnJson():
 def getUser(username):
     response = {}
     response['userName'] = username
+    return jsonify(response)
+
+@app.route('/CurrentCall/<string:username>', methods=['GET'])
+def getCurrentCall(username):
+    response = {
+        'priority':  PriorityLevel.HIGH.value, 
+        'title': 'Tree down on power line', 
+        'service': 'Electrical',
+        'address': '1000 Hilltop Cir, Baltimore, MD 21250',
+        'dateTime': '3:00 AM EST',
+        'locationDetails': 'West Hill Apartments',
+        'description': 'Power is out, large tree fell on power line.',
+        'phoneNumber': '(555) 555-5555'
+    }
     return jsonify(response)
 
 #my change
---
+++ users/priority_level.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class PriorityLevel(Enum):
+    HIGH = 'High'
+    MEDIUM = 'Medium'
+    LOW = 'Low'
