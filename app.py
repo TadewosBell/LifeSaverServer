f24e9b668101e448874f3e98a60a15e11ecca661
ssandif <22110419+ssandif@users.noreply.github.com>
2019-10-17 00:05:52 -0400

    Add mongoengine and mission and call APIs (#4)

--- app.py
+++ app.py
@@ -1,36 +1,90 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
+from flask_pymongo import pymongo,PyMongo
+from mongoengine import connect
+from config import *
+from models import *
+from datetime import datetime
+
 
 app = Flask(__name__)
-CORS(app)
+
+app.config.from_object("config")
+app.config['CORS_HEADERS'] = 'Content-Type'
+mongo = PyMongo(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+connect(db=MONGO_DBNAME, host=MONGO_URI)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 from users import userFunctions
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
-
-@app.route('/JsonObject', methods=['POST'])
-def returnJson():
+@app.route('/GetRequest/<string:user_name>',methods=['GET'])
+def getUser(user_name):
     response = {}
-    response['testString'] = '1'
-    response['testNumber'] = 1
-    response['testBool'] = False
-    response['functionReturn'] = userFunctions.simpleFunction()
+    user = userFunctions.User(user_name,'Tadewos','Bellete')
+    user.print()
+    user.register()
     return jsonify(response)
 
-@app.route('/GetRequest/<string:username>',methods=['GET'])
-def getUser(username):
-    response = {}
-    response['userName'] = username
-    return jsonify(response)
+@app.route('/SignUp', methods=['POST'])
+def SignUp():
+    content = request.get_json()
+    print(content)
+    user = userFunctions.User(content['email'], content['firstName'],content['lastName'],content['password'])
+    user.register()
+    return jsonify(content)
 
-@app.route('/hello')
-def hello():
-	return render_template('hello.html')
-	
+@app.route('/Missions', methods=['GET'])
+def get_missions():
+    return Mission.objects().to_json()
+
+@app.route('/Missions/<string:id>', methods=['GET'])
+def get_mission(id):
+    return Mission.objects(id=id).get().to_json()
+
+@app.route('/Missions', methods=['POST'])
+def post_mission():
+    content = request.get_json()
+    created = Mission().from_json(content)
+    created.save()
+    return '', 201
+
+@app.route('/Missions/<string:id>', methods=['DELETE'])
+def delete_mission(id):
+    Mission.objects(id=id).get().delete()
+    return '', 204
+
+@app.route('/Missions/<string:id>/Calls/<string:callId>', methods=['POST'])
+def post_call_to_mission(id, callId):
+    mission = Mission.objects(id=id).get()
+    call = Call.objects(id=callId).get()
+    if not mission or not call:
+        return '', 404
+    mission.calls.append(call)
+    mission.save()
+    return '', 204
+
+@app.route('/Calls', methods=['GET'])
+def get_calls():
+    return Call.objects().to_json()
+
+@app.route('/Calls/<string:id>', methods=['GET'])
+def get_call(id):
+    return Call.objects(id=id).to_json()
+
+@app.route('/Calls', methods=['POST'])
+def post_call():
+    content = request.get_json()
+    created = Call().from_json(content)
+    created.timeReceived = datetime.now()
+    created.save()
+    return '', 201
+
+@app.route('/Calls/<string:id>', methods=['DELETE'])
+def delete_call(id):
+    Call.objects(id=id).get().delete()
+    return '', 204
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
--- users/userFunctions.py
+++ users/userFunctions.py
@@ -1,7 +1,35 @@
 
+import pymongo
+from app import mongo
+import hashlib
 
-def simpleFunction():
-    response = {}
-    response['JsonObject'] = {}
-    response['JsonObject']['Variable'] = 'nestedVariable'
-    return response
+userDb = mongo.db.userInfo
+
+class User:
+    def __init__(self, email, firstName, lastName, password):
+        self.email = email
+        self.first_name = firstName
+        self.last_name = lastName
+        self.password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+    def register(self):
+        document = {'email':self.email,
+                    'first_name': self.first_name,
+                    'last_name': self.last_name,
+                    'password': self.password}
+        userDb.insert(document)
+    def print(self):
+        print(self.email)
+
+def login(email,password):
+    userData = userDb.find_one({'email', email})
+    loginPassword = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    if(userData != None):
+        if(userData['password'] == loginPassword):
+            print('login')
+            userData.pop('password', None)
+            return userData
+    else:
+        print('no user found')
+        #return a json object that has an error that is email not found
+
