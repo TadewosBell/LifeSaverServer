027eee997a4a14f0c6fa2aaea72766b8f882fdfb
Supreme Overlord of the Universe <16561590+MNchino@users.noreply.github.com>
2019-10-24 16:22:08 -0400

    Created branch and did a few things on Flask

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from flask_pymongo import pymongo,PyMongo
+from flask_login import LoginManager 
+
 
 app = Flask(__name__)
 
@@ -15,7 +17,7 @@ from users import userFunctions
 @app.route('/GetRequest/<string:user_name>',methods=['GET'])
 def getUser(user_name):
     response = {}
-    user = userFunctions.User(user_name,'Tadewos','Bellete')
+    user = userFunctions.User(user_name,'Tadewos','Bellete', 'password')
     user.print()
     user.register()
     return jsonify(response)
@@ -26,7 +28,15 @@ def SignUp():
     print(content)
     user = userFunctions.User(content['email'], content['firstName'],content['lastName'],content['password'])
     user.register()
+
     return jsonify(content)
+
+@app.route('/SignIn', methods=['POST'])
+def SignIn():
+    content = request.get_json()
+    print(content)
+    token = userFunctions.login(content['email'], content['password'])
+    return token
 
 if __name__ == '__main__':
    app.run(debug=True)
--- users/userFunctions.py
+++ users/userFunctions.py
@@ -2,34 +2,50 @@
 import pymongo
 from app import mongo
 import hashlib
+from flask import jsonify
+from flask_login import LoginManager 
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (create_access_token)
+from app import app
 
 userDb = mongo.db.userInfo
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 class User:
-    def __init__(self, email, firstName, lastName, password):
+    def __init__(self, email="", firstName="", lastName="", password=""):
         self.email = email
         self.first_name = firstName
         self.last_name = lastName
-        self.password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def register(self):
         document = {'email':self.email,
                     'first_name': self.first_name,
                     'last_name': self.last_name,
                     'password': self.password}
+        #Doesn't register same user twice
         userDb.insert(document)
+        #TODO HERE: MAKE THE ERROR VISIBLE TO THE USER THAT THE NAME IS ALREADY TAKEN
+
     def print(self):
         print(self.email)
 
-def login(email,password):
+def login(email="",password=""):
     userData = userDb.find_one({'email', email})
-    loginPassword = hashlib.sha256(password.encode('utf-8')).hexdigest()
     if(userData != None):
-        if(userData['password'] == loginPassword):
+        if(bcrypt.check_password_hash(userData['password'], password)):
             print('login')
             userData.pop('password', None)
-            return userData
+            access_token = create_access_token(identity = {
+                'first_name': userData['first_name'],
+                'last_name': userData['last_name'],
+                'email': userData['email'],
+            })
+            return jsonify({"token":access_token})      
+        else:
+            return jsonify({"result": "Incorrect password"})
     else:
         print('no user found')
-        #return a json object that has an error that is email not found
-
+        return jsonify({"result": "Email not found"})
