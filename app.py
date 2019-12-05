39e0fba20e7e7642dd8f389a9f64967b93002ba7
Tadewosbell <bell6@gmail.com>
2019-12-05 16:32:54 -0500

    changed ip

--- app.py
+++ app.py
@@ -227,4 +227,5 @@ def after_request(response):
   return response
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
