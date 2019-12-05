64f413f55475cee64f5fe2854dd8038ef7a1b1f1
Tadewosbell <bell6@gmail.com>
2019-12-05 16:20:46 -0500

    cors *

--- app.py
+++ app.py
@@ -220,7 +220,7 @@ def delete_User(email):
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', 'https://freeflowapp.com')
+  response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
