df46e6e20573d0f7c0fd84392a697b824db979d7
Supreme Overlord of the Universe <16561590+MNchino@users.noreply.github.com>
2019-12-05 15:46:24 -0500

    added something I won't use

--- app.py
+++ app.py
@@ -28,6 +28,13 @@ jwt = JWTManager(app)
 
 #from users import userFunctions
 
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
 @app.route('/GetRequest/<string:user_name>',methods=['GET'])
 def getUser(user_name):
     response = {}
